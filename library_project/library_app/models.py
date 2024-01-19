ffbc9bd8b2370611fcfccd36468120f305bb40a8
Denys Morozov <ajax3101@gmail.com>
2024-01-20 00:48:41 +0200

    add save img

--- library_project/library_app/models.py
+++ library_project/library_app/models.py
@@ -33,7 +33,7 @@ class Book(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, verbose_name = 'Жанр')
     release_year = models.PositiveIntegerField(verbose_name = 'Рік видання')
     is_read = models.BooleanField(default=False, verbose_name = 'Чек')
-    img = models.ImageField(upload_to='book_images/', blank=True, null=True, default="def.png")
+    img = models.ImageField(upload_to='book_images/', blank=True, null=True, default="noname.png", verbose_name = 'Картинка')
 
     def __str__(self):
         return self.title
