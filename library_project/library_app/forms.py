3974cd8593476cdcf0806ec8d8600cef71a8da06
Denys Morozov <ajax3101@gmail.com>
2024-01-20 01:21:42 +0200

    change is_read

--- library_project/library_app/forms.py
+++ library_project/library_app/forms.py
@@ -4,5 +4,5 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'genre', 'release_year']
+        fields = ['title', 'author', 'genre', 'release_year', 'is_read']
 
